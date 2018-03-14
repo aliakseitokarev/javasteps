1832923230f199ab78cf71ed3d1ca16007d496f1
Alex <aliaksei.tokarev@gmail.com>
2018-03-14 16:05:45 +0300

    user input

--- Calculate.java
+++ Calculate.java
@@ -2,10 +2,19 @@ public class Calculate {
 	
 	public static void main(String[] arg){
 		System.out.println("Calculating...");
-		int first = Integer.valueOf(arg[0]);
-		int second = Integer.valueOf(arg[1]);
-		int summ = first + second;
+		double first = Double.valueOf(arg[0]);
+		double second = Double.valueOf(arg[1]);
+		double summ = first + second;
+		double subt = first - second;
+		double mult = first*second;
+		double div = first/second;
+		int deg = (int) Math.pow(first, second);
 		System.out.println("Sum " + summ);
+		System.out.println("Subtraction " + subt);
+		System.out.println("Multiplication " + mult);		
+		System.out.println("Division " + div);
+		System.out.println("Elevate " + deg);
+		
 	} 
 
 }
