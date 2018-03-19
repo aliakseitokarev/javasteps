ac68b265fa305e024beae981d0e16fbc883f74de
Alex <aliaksei.tokarev@gmail.com>
2018-03-19 17:09:06 +0300

    рабочая реализация

--- src/main/java/by/learn/lessons/Calculator.java
+++ src/main/java/by/learn/lessons/Calculator.java
@@ -1,34 +1,100 @@
 package by.learn.lessons;
+
+import java.util.Scanner;
+
 /**
- Класс реализует калькулятор.
+ * Класс реализует калькулятор.
  */
 public class Calculator {
 
-    private int result; //Результат вычисления
+    private double result; //result of calculation
 
     /**
-     Суммирует аргументы.
-     @params params Аргументы суммирования.
+     * Summing args
+     * @param first - arg #1
+     * @param second - arg #2
      */
-    public void add(int ... params) {
-        for (Integer param : params){
-            this.result += param;
+    public void add(double first, double second) {
+        this.result = first+second;
         }
+
+    /**
+     * Subtractions args
+     * @param first - arg #1
+     * @param second - arg #2
+     */
+    public void subt(double first, double second) {
+        this.result = first-second;
     }
 
     /**
-     Получает результат.
-     @return результат вычисления.
+     * Multiplication args
+     * @param first - arg #1
+     * @param second - arg #2
      */
-    public int getResult() {
+    public void mult (double first, double second) {
+        this.result = first*second;
+    }
+
+     /**
+      * Division args
+      * @param first - arg #1
+      * @param second - arg #2
+      */
+     public void div (double first, double second) {
+        this.result = first/second;
+    }
+
+    /**
+     * Get result
+     * @return result of calculation
+     */
+    public double getResult() {
         return this.result;
     }
 
     /**
-     Очистить результат вычисления.
+     * Clean result
      */
-
     public void cleanResult() {
         this.result = 0;
     }
+
+
+    public static String getOperation () {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter required operation (add, subt, mult, div) : ");
+        String choice = reader.next();
+        return choice;
+    }
+
+    /**
+     * Switch between operations and return result
+     * @param operation add/subt/mult/div
+     * @param first - arg #1
+     * @param second - arg #2
+     * @return result of calculation
+     */
+    public double switcher (String operation, double first, double second){
+
+            switch (operation.toLowerCase()){
+                case "add":
+                    this.add(first, second);
+                    break;
+                case "subt":
+                    this.subt(first, second);
+                    break;
+                case "mult":
+                    this.mult(first, second);
+                    break;
+                case "div":
+                    this.div(first, second);
+                    break;
+                default:
+                    System.out.println("Wrong operation");
+                    result = switcher(getOperation(), first, second);
+        }
+        return this.getResult();
+    }
+
 }
--- src/main/java/by/learn/lessons/InteractRunner.java
+++ src/main/java/by/learn/lessons/InteractRunner.java
@@ -12,21 +12,57 @@ public class InteractRunner {
         try {
             Calculator calc = new Calculator();
             String exit = "no";
-            while (!exit.equals("yes"))
-            {
-                System.out.println ("Enter first arg : ");
-                String first = reader.next();
+            double first = 0.0;
+            while (!exit.equals("yes")) {
+                // check saved result
+                if (first == 0) {
+                    System.out.println("Enter the first arg : ");
+                    first = Double.valueOf(reader.next()); //первый аргумент из консоли
+                }
                 System.out.println ("Enter second arg : ");
-                String second = reader.next();
-                calc.add(Integer.valueOf(first),Integer.valueOf(second));
+                double second = Double.valueOf(reader.next()); //второй аргумент из консоли
+
+                //System.out.println ("Enter required operation (add, subt, mult, div) : ");
+                //String choice = reader.next();
+                //
+
+
+
+                /**
+                 * сделать проверку вводимого значения требуемой операции - пока
+                 * не знаю как (либо считать последнее сообщение из консоли либо
+                 * либо как-то еще
+                 */
+
+                calc.switcher(calc.getOperation(), first, second);
+
+                //попытка считать последнее сообщение которое было выведено в консоль...
+                //boolean console = System.in.equals("Wrong operation");
+                //System.out.println(console);
+
                 System.out.println ("Result : " + calc.getResult());
-                calc.cleanResult();
+
                 System.out.println ("Exit : yes/no ");
-                exit = reader.next();
+                exit = reader.next().toLowerCase();
+                if (exit.equals("no")){
+                    System.out.println ("Do you want to save the result?: yes/no");
+                    //сохранение в памяти результата
+                    if (reader.next().toLowerCase().equals("yes"))
+                        first = calc.getResult();
+                    else first =0;
+                }
+                //очищение результата
+                calc.cleanResult();
             }
         } finally {
             reader.close();
         }
-
     }
+
+ /*   public static String getOperation () {
+        Scanner reader = new Scanner(System.in);
+        System.out.println ("Enter required operation (add, subt, mult, div) : ");
+        String choice = reader.next();
+        return choice;
+    }*/
 }
